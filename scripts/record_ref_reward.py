4bc8b3d6077f4e688a2c32fb62b69d38532e951b
Pengwei Sun <pengwei@stanford.edu>
2025-05-26 01:26:02 -0700

    ''

---
+++ scripts/record_ref_reward.py
@@ -0,0 +1,54 @@
+import torch
+from torch import nn, optim
+import sys
+sys.path.append("..")
+from transformers import AutoModelForCausalLM
+from data.dpo_ultrafeedback_data_pipeline import get_dataloader, tokenizer, MODEL_NAME
+from tqdm import tqdm
+import os
+from peft import get_peft_model, LoraConfig, TaskType
+from torch.utils.tensorboard import SummaryWriter
+from datetime import datetime
+from utils import dpo_loss, linear_warmup_schedule, sequence_log_prob
+from torch.optim.lr_scheduler import LambdaLR
+import argparse
+
+run_name = datetime.now().strftime("%Y%m%d-%H%M%S")
+
+
+
+def finetune_model():
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    ref_model_path = '../checkpoints/preference_sft_20250520-041117/step_55000'
+    ref_model = AutoModelForCausalLM.from_pretrained(ref_model_path, trust_remote_code=True)
+    ref_model.config.pad_token_id = tokenizer.pad_token_id
+    ref_model.to(device)
+    ref_model.eval()
+    train_dataloader = get_dataloader(split="train_prefs", batch_size=args.batch_size, shuffle=False, num_workers=args.num_workers, debug_mode=args.debug_mode)
+
+    global_step = 0
+    confidences = []
+    pbar = tqdm(train_dataloader)
+    for batch in pbar:
+        batch = {k: v.to(device) for k, v in batch.items()}
+        ref_log_prob_chosen = sequence_log_prob(ref_model, batch['input_ids_chosen'], batch['attention_mask_chosen'], batch['labels_chosen'])
+        ref_log_prob_rejected = sequence_log_prob(ref_model, batch['input_ids_rejected'], batch['attention_mask_rejected'], batch['labels_rejected'])
+        ref_confidence = ref_log_prob_chosen - ref_log_prob_rejected
+        confidences.append(ref_confidence.item())
+        pbar.set_postfix({"confidence": ref_confidence.item()})
+        global_step += 1
+
+    output_dir = f"../output/confidences_ref.txt"
+    with open(output_dir, 'w') as f:
+        for confidence in confidences:
+            f.write(f"{confidence}\n")
+    print(f"Confidences saved to {output_dir}")
+def parse_args():
+    parser = argparse.ArgumentParser(description="Fine-tune a causal LM with DPO data.")
+    parser.add_argument("--batch_size", type=int, default=1, help="Batch size for training and evaluation.")
+    parser.add_argument("--num_workers", type=int, default=1, help="Number of workers for data loading.")
+    return parser.parse_args()
+
+if __name__ == "__main__":
+    args = parse_args()
+    finetune_model()
