4e1cfb27d52c0166e204e68efb8bf7f72bfa46e1
Pengwei Sun <pengwei@stanford.edu>
2025-05-27 16:24:18 -0700

    ''

---
+++ scripts/leaderboard_submission.py
@@ -0,0 +1,22 @@
+import json
+from vllm import LLM, SamplingParams
+
+prompts = []
+with open("ultrafeedback_leaderboard.json", 'r') as f:
+    for line in f:
+        prompts.append(line.strip()['prompt'])
+    # data = json.load(f)
+
+sampling_params = SamplingParams(temperature=0.3, max_tokens=989)
+llm = LLM(model='../checkpoints/preference_dpo_20250526_new_sft/step_30000', dtype="auto")
+outputs = llm.generate(prompts, sampling_params)
+your_responses = [o.outputs[0].text.strip() for o in outputs]
+
+
+with open("leaderboard_submission.json", "w") as f:
+    for prompt, response in zip(prompts, your_responses):
+        result = {}
+        result['prompt'] = prompt
+        result['response'] = response
+        f.write(json.dump(result) + '\n')
+print("Saved your model responses to your_outputs.json")
