42a7130e2fa03aa81dc14b0f1e0dba416f6fbb46
Pengwei Sun <pengwei@stanford.edu>
2025-05-30 13:12:44 -0700

    ''

--- scripts/leaderboard_submission.py
+++ scripts/leaderboard_submission.py
@@ -9,10 +9,10 @@ with open("ultrafeedback_leaderboard.json", 'r') as f:
         prompts.append(data['prompt'])
 
 sampling_params = SamplingParams(temperature=1, max_tokens=1024)
-llm = LLM(model='../checkpoints/preference_dpo_20250526_new_sft/step_30000')
+llm = LLM(model='../checkpoints/preference_sft_20250525_pref_sft_grad_acc_length_600/step_55000')
 outputs = llm.generate(prompts, sampling_params)
 your_responses = [o.outputs[0].text.strip() for o in outputs]
-print(your_response)
+# print(your_response)
 with open("leaderboard_submission.json", "w") as f:
     for prompt, response in zip(prompts, your_responses):
         result = {}
